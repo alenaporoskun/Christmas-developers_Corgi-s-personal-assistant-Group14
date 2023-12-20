de43e1b113b02f1a4c15a307eadb9d5c755a6692
Olena Poroskun <56368022+alenaporoskun@users.noreply.github.com>
2023-12-20 12:50:52 +0200

    Update setup.py

--- console_assistant/setup.py
+++ console_assistant/setup.py
@@ -4,7 +4,7 @@ setup(
     name='console_assistant',
     version='0.1',
     description="A very useful console assistant for Santa's assistant, Mr. Corgi.",
-    url='https://github.com/alenaporoskun/Christmas-developers_Santa-s-personal-assistant',
+    url='https://github.com/alenaporoskun/Christmas-developers_Corgi-s-personal-assistant-Group14',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
